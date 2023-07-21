ce6e27b5aaf887cad9f6422b8df002061c17a65a
Robert Pettis <pettis.robert@gmail.com>
2023-07-21 15:07:26 -0700

    adding abridged required packages to setup.py and removing requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
-# Read in requirements.txt
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setup(
     name="pywrangling",
-    version="0.11",
+    version="0.1",
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=[
+        'pandas',
+        'numpy',
+        'inspec',
+        'tqdm',
+    ],
 )
