3b0e1a0334ac77a44aeb285ae56b5040d08d9f8e
Robert Pettis <pettis.robert@gmail.com>
2023-07-21 14:37:51 -0700

    removed dash from package name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Required to turn the repo into a package.
 from setuptools import setup, find_packages
 
 setup(
-    name="py-wrangling",
+    name="pywrangling",
     version="0.1",
     packages=find_packages(),
 )
