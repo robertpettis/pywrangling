8c35d37e88cb2e6097d0fb2c3c417e0fcb62b46f
Robert Pettis <pettis.robert@gmail.com>
2023-07-21 14:35:21 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Required to turn the repo into a package.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="py-wrangling",
+    version="0.1",
+    packages=find_packages(),
+)
+
+
