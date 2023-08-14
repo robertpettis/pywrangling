6317d25bd102bfc574f67c8b79f2c59252582810
Robert Pettis <pettis.robert@gmail.com>
2023-08-14 11:17:10 -0700

    Added a function to initialize browser for crawling with basic settings.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywrangling",
-    version="0.4.5",
+    version="0.5.0",
     packages=find_packages(),
     install_requires=[
         'pandas',
