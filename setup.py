6fe3174446e82af35b181986bbe96f7e2359028e
Robert Pettis <pettis.robert@gmail.com>
2023-08-21 10:33:07 -0700

    Added a wait to credential entry.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywrangling",
-    version="0.5.10",
+    version="0.5.11",
     packages=find_packages(),
     install_requires=[
         'pandas',
