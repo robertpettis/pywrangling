b739774016c35f1c8e04f85e1ff283b349472392
Robert Pettis <pettis.robert@gmail.com>
2023-08-25 15:17:21 -0700

    correcting some unexpected behavior of the replace function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywrangling",
-    version="0.6.4.5",
+    version="0.6.4.7",
     packages=find_packages(),
     install_requires=[
         'pandas',
