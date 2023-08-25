2fb6fd149fcb42cc007007fa17affb7ffdbfc2fd
Robert Pettis <pettis.robert@gmail.com>
2023-08-25 15:51:17 -0700

    more duplicate fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywrangling",
-    version="0.6.4.9",
+    version="0.6.4.10",
     packages=find_packages(),
     install_requires=[
         'pandas',
