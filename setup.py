5dc098b523c9024038f340ce087f158f23d9b003
Robert Pettis <pettis.robert@gmail.com>
2023-10-19 13:37:37 -0700

    added a function which manages user input given it was run by batch or otherwise

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywrangling",
-    version="0.11.3",
+    version="0.12.0",
     packages=find_packages(),
     install_requires=[
         'pandas',
