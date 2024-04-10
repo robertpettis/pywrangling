04c643bd119a2908aacc9b5a0a4735cd361b2103
Robert Pettis <pettis.robert@gmail.com>
2024-04-10 10:03:41 -0700

    Added functions to make an 'Other' and a 'Total' row

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywrangling",
-    version="0.29",
+    version="0.30",
     packages=find_packages(),
     install_requires=[
         'pandas',
