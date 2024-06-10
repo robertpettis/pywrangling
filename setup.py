a468a0ab5cc981920211cd789fb1d429c89a043d
Robert Pettis <pettis.robert@gmail.com>
2024-06-10 14:55:16 -0400

    debug: depersonalize function to act as a string

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywrangling",
-    version="0.35",
+    version="0.35.1",
     packages=find_packages(),
     install_requires=[
         'pandas',
