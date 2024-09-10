749ae46b086b994529c0687dda6b333bcd00053e
Robert Pettis <pettis.robert@gmail.com>
2024-09-10 10:47:35 -0400

    add: added monkey patched values_and_percent method to be used like a value_counts method, except it ALSO includes the percentages as a second column

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywrangling",
-    version="0.37.3",
+    version="0.38",
     packages=find_packages(),
     install_requires=[
         'pandas',
