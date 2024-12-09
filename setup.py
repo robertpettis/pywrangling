e69df36accdac70923fe6eb74b34781e25b8e9d7
Robert Pettis <pettis.robert@gmail.com>
2024-12-09 15:29:20 -0500

    Updating comment in gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywrangling",
-    version="0.42.3.0",
+    version="0.42.3.1",
     packages=find_packages(),
     install_requires=[
         'pandas',
