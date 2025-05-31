eba6bcedd658d90a023b4e6595460590bbd6c548
Robert Pettis <pettis.robert@gmail.com>
2025-05-31 17:17:37 -0400

    bug fix: getting error for time package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywrangling",
-    version="0.45.3",
+    version="0.46.1",
     packages=find_packages(),
     install_requires=[
         'pandas',
