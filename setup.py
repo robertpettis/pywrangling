3acfc43f7d1b6ec31fb62eb2a52ffc6ff7149301
Robert Pettis <pettis.robert@gmail.com>
2025-06-26 11:09:43 -0400

    Fix: move_column now works when there are duplicate column names by informing you, then adding an index to the column names. It can also return a list of the column names changed Modify: move_column now works with a list of columns

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywrangling",
-    version="0.47",
+    version="0.48",
     packages=find_packages(),
     install_requires=[
         'pandas',
