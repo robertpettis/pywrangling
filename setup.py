03b5eb653dd4cd63d545a34733e29624c0008cef
Robert Pettis <pettis.robert@gmail.com>
2026-02-09 08:58:12 -0500

    bug fixes for clicker remap

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywrangling",
-    version="0.53.1",
+    version="0.53.2",
     packages=find_packages(),
     install_requires=[
         'pandas',
