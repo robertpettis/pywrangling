83fbbd4c8530a6c2284a3cc76d564050a44fd0c0
Robert Pettis <pettis.robert@gmail.com>
2026-02-09 07:52:39 -0500

    correcting version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywrangling",
-    version="0.52",
+    version="0.53",
     packages=find_packages(),
     install_requires=[
         'pandas',
